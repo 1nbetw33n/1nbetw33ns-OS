c70c533ca2c5abd6fb6860a6a49c09934e482b25
inbetween <a.ab@mail.de>
2023-04-21 18:03:35 +0200

    added comment

--- src/script/install.sh
+++ src/script/install.sh
@@ -6,6 +6,7 @@ modules_dir="modules"
 # read module names from file into array
 readarray -t modules < "${resources_dir}/modules.csv"
 
+# run modules
 for module in "${modules[@]}"; do
   bash "${modules_dir}/${module}"
 done

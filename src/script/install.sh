f4ce0adb6d54fc1d99e832f227aa7aefd282345b
inbetween <a.ab@mail.de>
2023-04-25 10:25:52 +0200

    updated wording

--- src/script/install.sh
+++ src/script/install.sh
@@ -6,7 +6,8 @@ modules_dir="modules"
 # read module names from file into array
 readarray -t modules < "${resources_dir}/modules.csv"
 
-# run modules
+# executing modules
 for module in "${modules[@]}"; do
   bash "${modules_dir}/${module}"
 done
+

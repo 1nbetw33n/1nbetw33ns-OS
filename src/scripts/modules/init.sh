788c3ffa0e2b0814294622eff68ae933c1b9e59d
inbetween <a.ab@mail.de>
2023-06-25 19:21:26 +0200

    updated way to install powerlevel10k because the package from aur is broken

--- src/scripts/modules/init.sh
+++ src/scripts/modules/init.sh
@@ -34,3 +34,8 @@ sudo sed -i 's/#WaylandEnable=false/WaylandEnable=false\nDefaultSession=gnome-xo
 # setting up zsh
 echo "### Core :: Installing Oh-my-zsh... ###"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" & disown
+zsh
+
+# installing powerlevel10k
+echo "### Core :: Installing Powerlevel10k... ###"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/themes/powerlevel10k
